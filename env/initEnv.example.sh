4e1b46bc575fa087ba8102d1ec1bbedebfad825a
Brice Vandeputte <brice.vandeputte@orange.com>
2020-10-04 15:52:15 +0200

    Fix #8 add admin database
    - move schema into dedicated folder
    - add contribution schema
    - add hasAdminRole()
    - add hasOwnerRole()

--- env/initEnv.example.sh
+++ env/initEnv.example.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # create mongo db free plan from https://cloud.mongodb.com/
 export JARDI_MONGO_URI="mongodb+srv://userHere:passwordHere@clusterHere.xzryx.mongodb.net/dbNameHere?retryWrites=true&w=majority"
+# export JARDI_ADMIN_MONGO_DB_NAME=jardinAdmin
