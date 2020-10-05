952e35db8fb3da0cf0d67284d9f6af29bbddc4d7
Brice Vandeputte <brice.vandeputte@orange.com>
2020-10-05 13:39:25 +0200

    Fix #10 jt: contribution show

--- env/initEnv.example.sh
+++ env/initEnv.example.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # create mongo db free plan from https://cloud.mongodb.com/
-export JARDI_MONGO_URI="mongodb+srv://userHere:passwordHere@clusterHere.xzryx.mongodb.net/dbNameHere?retryWrites=true&w=majority"
+# export JARDI_MONGO_URI="mongodb+srv://userHere:passwordHere@clusterHere.xzryx.mongodb.net/dbNameHere?retryWrites=true&w=majority"
+JARDI_MONGO_URI=mongodb://127.0.0.1:27017/jardin?retryWrites=true&w=majority
 # export JARDI_ADMIN_MONGO_DB_NAME=jardinAdmin
